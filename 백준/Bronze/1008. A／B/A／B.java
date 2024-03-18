413b7189622d7d16378e9ba1ef9cd06835bb266e
ddochiisrich <157680907+ddochiisrich@users.noreply.github.com>
2024-03-18 19:00:49 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double A = scan.nextInt();
+		double B = scan.nextInt();
+		
+		if(A > 0 && B > 0) {
+
+		System.out.print(A/B);
+		
+		}
+	}
+}
