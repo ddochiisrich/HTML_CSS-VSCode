4c1e1861188eec6e7997d70f1f2a49fe0fcd563a
ddochiisrich <157680907+ddochiisrich@users.noreply.github.com>
2024-03-20 15:51:56 +0900

    [Bronze V] Title: 꼬마 정민, Time: 204 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		long A = scan.nextLong();
+		long B = scan.nextLong();
+		long C = scan.nextLong();    
+        long D = A+B+C;
+		
+		System.out.print(D);
+	}
+}
